85fb89dbbeb1eb9f6e1d76f5b3f2df33c0902dd1
Wanessa Silva <wanessa.silva33@aluno.ce.gov.br>
2025-11-15 20:57:37 +0000

    Projeto concluído ✭

---
+++ index.js
@@ -0,0 +1,65 @@
+let tecnica1 = {
+    nome: "Chiaroescuro",
+    descricao: "O chiaroscuro usa o contraste forte entre luz e sombra para criar profundidade e impacto dramático na obra.",
+    pros: "Aumenta o realismo ao criar profundidade e volume.",
+    contras: "Pode esconder detalhes e dificultar a leitura da obra."
+}
+
+let tecnica2 = {
+    nome: "Aquarela",
+    descricao: "Técnica feita com pigmentos diluídos em água, aplicada em camadas transparentes sobre o papel.",
+    pros: "Leve, rápida e produz efeitos delicados.",
+    contras: "Difícil de corrigir e difícil controlar manchas."
+}
+
+let tecnica3 = {
+    nome: "Óleo",
+    descricao: "Tinta feita com pigmentos misturados a óleo, permitindo secagem lenta e misturas profundas.",
+    pros: "Permite retoques e misturas ricas, criando acabamento detalhado.",
+    contras: "Seca muito devagar e exige muitos materiais."
+}
+
+let tecnica4 = {
+    nome: "Acrilica",
+    descricao: "Tinta à base de água que seca rapidamente, sendo versátil e fácil de usar.",
+    pros: "Barata, versátil e fácil de limpar.",
+    contras: "Seca rápido demais, dificultando misturas suaves."
+}
+
+let tecnica5 = {
+    nome: "Guache",
+    descricao: "Tinta opaca à base de água, mais espessa e com maior cobertura que a aquarela.",
+    pros: "Cores vivas e ótima cobertura.",
+    contras: "Pode rachar em camadas grossas e reativa com água."
+}
+
+let tecnica6 = {
+    nome: "Pastel seco",
+    descricao: "Bastões de pigmento seco aplicados diretamente no papel, com textura e cor intensas.",
+    pros: "Cores fortes e textura rica.",
+    contras: "Sujeira fácil e necessidade de fixador."
+}
+
+let tecnica7 = {
+    nome: "Tempera",
+    descricao: "Técnica feita com pigmentos misturados a emulsões, como gema de ovo, resultando em secagem rápida.",
+    pros: "Alta durabilidade e secagem rápida.",
+    contras: "Pouca flexibilidade de cor e difícil de corrigir."
+}
+
+let tecnicas = [tecnica1, tecnica2, tecnica3, tecnica4, tecnica5, tecnica6, tecnica7]
+
+function mostrarTecnicas(tecnicas) {
+    console.log("--==|´Técnicas de pintura,|==--")
+  for (let i = 0; i < tecnicas.length; i++) {
+    console.log(`\n=== Técnica ${i + 1}: ${tecnicas[i].nome} ===
+
+Descrição: ${tecnicas[i].descricao}
+
+Prós: ${tecnicas[i].pros}
+Contras: ${tecnicas[i].contras}
+=============================`)
+  }
+}
+
+mostrarTecnicas(tecnicas);
